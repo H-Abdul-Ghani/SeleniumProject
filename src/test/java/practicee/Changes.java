1a6107a988aa96fa05d8782c096ccda1ff6616dd
DELL <DELL@DESKTOP-6VKNEBP>
2026-04-13 12:00:59 +0530

    chanagess

--- src/test/java/practicee/Changes.java
+++ src/test/java/practicee/Changes.java
@@ -3,6 +3,7 @@ package practicee;
 public class Changes {
 	public static void main(String[] args) {
 		System.out.println("This class is for changes");
+		System.out.println("*888888***");
 	}
 
 }
